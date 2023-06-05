cbb6b46cc803f6a77f636998e8a5b71654bb1865
ivansanchezrg <ivansanchezrg@gmail.com>
2023-06-05 09:49:28 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  {path:'login', component: LoginComponent },
+  {path:'cursos', component: CursosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
